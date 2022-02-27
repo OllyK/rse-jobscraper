48d93c849cc24e30a696993d6d46252cdfd3c595
Olly King <olly.king@diamond.ac.uk>
2022-02-27 19:10:10 +0000

    Fixes EMBL EBI scraper

--- ebi_job_finder.py
+++ ebi_job_finder.py
@@ -4,7 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 class EbiJobFinder:
 
-    base_page_url = "https://www.ebi.ac.uk/about/jobs"
+    base_page_url = "https://www.embl.org/jobs/ebi/iframe"
     
     def __init__(self, driver):
         self.driver = driver
@@ -14,35 +14,25 @@ class EbiJobFinder:
         self.driver.get(self.base_page_url)
         time.sleep(3)
         self.append_job_divs(self.driver)
-        # Click on the more jobs link
-        more_jobs = True
-        while more_jobs:
-            try:
-                link_wrapper = self.driver.find_element_by_xpath(".//li[contains(@class, 'pager-next')]")
-                link = link_wrapper.find_element_by_xpath(".//a")
-                self.driver.get(link.get_attribute("href"))
-                time.sleep(2)
-                self.append_job_divs(self.driver)
-            except NoSuchElementException as e:
-                more_jobs = False
         print(f"{len(self.job_divs)} EMBL-EBI jobs found.")
         return self.create_output_string(self.job_divs)
 
     def append_job_divs(self, driver):
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
-        list_items = soup.find_all("div", {"class": "job-item-row"})
+        list_items = soup.find_all("article", {"class": "vf-summary--job"})
         for item in list_items:
             self.job_divs.append(item)
 
     def create_output_string(self, jobs_list):
+        stem = "https://www.embl.org"
         output_string = "EMBL-EBI Jobs\n"
         for count, job in enumerate(jobs_list, start=1):
-            title_div = job.find("div", class_="views-field-nothing-1")
-            job_title = title_div.text.strip()
-            vacancy_url = title_div.find("a", href=True)['href']
+            url = job.find("a", class_="vf-summary__link", href=True)
+            job_title = url.text.strip()
+            vacancy_url = url["href"]
             output_string += (f"{count}: {job_title}\n")
-            output_string += (f"{vacancy_url}\n\n")
+            output_string += (f"{stem + vacancy_url}\n\n")
         return output_string
 
 # from utils import get_webdriver
