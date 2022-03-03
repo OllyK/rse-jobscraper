7e4273aa00cc36544b303157382d85182842621a
Olly King <olly.king@diamond.ac.uk>
2022-03-03 22:01:24 +0000

    Fixes Exscientia JobScraper

--- exscientia_job_finder.py
+++ exscientia_job_finder.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import time
+import json
 
 class ExscientiaJobFinder:
 
@@ -13,22 +14,27 @@ class ExscientiaJobFinder:
         time.sleep(3)
         html = self.driver.page_source
         soup = BeautifulSoup(html, "html.parser")
-        self.jobs = soup.find_all("li", {"class": "_70bb"})
-        print(f"{len(self.jobs)} Exscientia jobs found.")
-        return self.create_output_string(self.jobs)
+        self.driver.quit()
+        json_out = soup.find('script', id="__NEXT_DATA__")
+        script = json_out.contents[0].string
+        data = json.loads(script)
+        jobs = data['props']['pageProps']['allJobs']
+        print(f"{len(jobs)} Exscientia jobs found.")
+        return self.create_output_string(jobs)
 
     def create_output_string(self, jobs_list):
         output_string = "Exscientia Jobs\n"
         for count, job in enumerate(jobs_list, start=1):
-            vacancy_urls = job.find("a", href=True)
-            job_title = job.find("h2", class_="af77").text.strip()
-            location = job.find("h6", class_="_957f").text.strip()
+            vacancy_urls = job['shortlink']
+            job_title = job['full_title']
+            location = job['location']['location_str']
             output_string += (f"{count}: {job_title}\n")
             output_string += (f"{location}\n")
-            output_string += (f"{vacancy_urls['href']}\n\n")
+            output_string += (f"{vacancy_urls}\n\n")
         return output_string
 
 # from utils import get_webdriver
 # driver = get_webdriver()
 # exsci_job = ExscientiaJobFinder(driver)
 # print(exsci_job.find_jobs())
+# driver.quit()
